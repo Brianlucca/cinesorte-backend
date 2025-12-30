0fe771e947d5e08175fcbd179b39ec98ce876586
Brian Lucca <brianlucca2002@gmail.com>
2025-12-30 17:27:02 -0300

    refactor(routes): cleanup user routes and remove legacy list endpoints

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,18 +1,11 @@
 const express = require('express');
 const router = express.Router();
-
 const authController = require('../controllers/authController');
-const listController = require('../controllers/listController');
 const { verifyToken } = require('../middleware/authMiddleware');
 
 router.post('/register', authController.register);
 router.post('/login', authController.login);
 router.post('/logout', authController.logout);
 router.get('/me', verifyToken, authController.getMe);
-
-router.post('/lists', verifyToken, listController.saveList);
-router.get('/lists', verifyToken, listController.getLists);
-router.delete('/lists/:listId', verifyToken, listController.deleteList);
-router.delete('/lists/:listId/movies/:movieId', verifyToken, listController.removeMovieFromList);
 
 module.exports = router;
