4d820dd1ff5f5eb948fc0d5847f727711435dfd1
Brian Lucca <brianlucca2002@gmail.com>
2025-12-30 17:26:59 -0300

    refactor(middleware): enforce email verification check in auth middleware

--- src/middleware/authMiddleware.js
+++ src/middleware/authMiddleware.js
@@ -3,15 +3,20 @@ const { auth } = require('../config/firebase');
 const verifyToken = async (req, res, next) => {
   const token = req.cookies.authToken;
   if (!token) {
-    return res.status(403).json({ message: 'Acesso não autorizado. Nenhum token encontrado.' });
+    return res.status(403).json({ message: 'Unauthorized access' });
   }
 
   try {
     const decodedToken = await auth.verifyIdToken(token);
+    
+    if (!decodedToken.email_verified) {
+      return res.status(403).json({ message: 'Email verification required' });
+    }
+
     req.user = decodedToken;
     next();
   } catch (error) {
-    return res.status(403).json({ message: 'Token inválido ou expirado.' });
+    return res.status(403).json({ message: 'Invalid or expired token' });
   }
 };
 
