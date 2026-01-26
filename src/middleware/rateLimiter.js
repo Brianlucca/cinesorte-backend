03fd456ad00108c5cef920fc8b19a7d1f828d0ed
Brian Lucca <brianlucca2002@gmail.com>
2026-01-26 16:03:53 -0300

    feat: add rate limiter middleware

--- src/middleware/rateLimiter.js
+++ src/middleware/rateLimiter.js
@@ -2,7 +2,7 @@ const rateLimit = require('express-rate-limit');
 
 const tmdbApiLimiter = rateLimit({
   windowMs: 1 * 60 * 1000,
-  max: 30,
+  max: 100,
   standardHeaders: true,
   legacyHeaders: false,
   message: {
