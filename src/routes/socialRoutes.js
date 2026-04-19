1ead19937975b711c3f4cba2c4ccc600109eb03c
Brian Lucca <brianlucca2002@gmail.com>
2026-04-19 12:19:16 -0300

    chore: update src/routes/socialRoutes.js

--- src/routes/socialRoutes.js
+++ src/routes/socialRoutes.js
@@ -6,7 +6,7 @@ const feedController = require('../controllers/feedController');
 const listController = require('../controllers/listController');
 const { verifyToken, optionalVerify } = require('../middleware/auth');
 const validate = require('../middleware/validate');
-const { reviewSchema, commentSchema } = require('../schemas/schemas');
+const { reviewSchema, reviewUpdateSchema, commentSchema, commentUpdateSchema } = require('../schemas/schemas');
 
 router.get('/feed/global', verifyToken, feedController.getGlobalFeed);
 router.get('/feed/following', verifyToken, feedController.getFollowingFeed);
@@ -23,14 +23,14 @@ router.get('/followers/:userId', verifyToken, socialController.getUserFollowersL
 router.get('/following/:userId', verifyToken, socialController.getUserFollowingList);
 
 router.post('/reviews', verifyToken, validate(reviewSchema), reviewController.addReview);
-router.put('/reviews/:reviewId', verifyToken, reviewController.updateReview);
+router.put('/reviews/:reviewId', verifyToken, validate(reviewUpdateSchema), reviewController.updateReview);
 router.delete('/reviews/:reviewId', verifyToken, reviewController.deleteReview);
 router.get('/reviews/:mediaId', verifyToken, reviewController.getMediaReviews);
 router.get('/user-reviews/:username', verifyToken, reviewController.getUserReviews);
 router.post('/reviews/:reviewId/like', verifyToken, reviewController.toggleLikeReview);
 
 router.post('/comments', verifyToken, validate(commentSchema), reviewController.addComment);
-router.put('/comments/:commentId', verifyToken, reviewController.updateComment);
+router.put('/comments/:commentId', verifyToken, validate(commentUpdateSchema), reviewController.updateComment);
 router.delete('/comments/:commentId', verifyToken, reviewController.deleteComment);
 router.get('/comments/:reviewId', verifyToken, reviewController.getComments);
 
